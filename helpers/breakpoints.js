bb84d5378e401539a7c59f0d95ac6261ed4ce522
Willis Allstead <willis.allstead@noblestudios.com>
2022-02-09 09:00:25 -0800

    Some initial cleanup

---
+++ helpers/breakpoints.js
@@ -0,0 +1,7 @@
+const breakpoints = {
+    small: '@media only screen and (min-width: 768px)',
+    medium: '@media only screen and (min-width: 1024px)',
+    large: '@media only screen and (min-width: 1440px)'
+}
+
+export default breakpoints; 
