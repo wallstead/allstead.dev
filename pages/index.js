1b8d7242d2ecdca57ef3da7286b247ca89d8377f
Willis Allstead <willis.allstead@noblestudios.com>
2022-02-09 12:25:02 -0800

    Got card hover effect mostly working

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import Head from 'next/head'
-import React from 'react'
-import styled from 'styled-components';
-import breakpoints from '../helpers/breakpoints';
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useSpring, animated } from "react-spring";
+import useMeasure from "react-use-measure";
+import breakpoints from "../helpers/breakpoints";
 
 const Grid = styled.main`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  gap: 40px;
-  padding: 40px;
+  gap: 20px;
+  padding: 20px;
 
   ${breakpoints.medium} {
     grid-template-columns: 1fr 1fr 1fr;
+    gap: 40px;
+    padding: 40px;
   }
 
   ${breakpoints.large} {
@@ -18,20 +22,54 @@ const Grid = styled.main`
   }
 `;
 
-const GridItem = styled.div`
+const GridItem = styled(animated.div)`
   background-color: white;
   border-radius: 27px;
-  aspect-ratio: 1;
+  aspect-ratio: 4 / 3;
   display: flex;
   justify-content: center;
+  grid-column: ${(props) => (props.columnStart ? props.columnStart : "auto")} /
+    ${(props) => (props.columnEnd ? props.columnEnd : "auto")};
+  grid-row: ${(props) => (props.rowStart ? props.rowStart : "auto")} /
+    ${(props) => (props.rowEnd ? props.rowEnd : "auto")};
+  transform-style: preserve-3d;
+  position: relative;
 
-  grid-column-start: ${props => props.columnStart};
-  grid-column-end: ${props => props.columnEnd};
-  grid-row-start: ${props => props.rowStart};
-  grid-row-end: ${props => props.rowEnd};
+  &::before {
+    content: '';
+    background: #e7e1cb;
+    position: absolute;
+    height: 100%;
+
+    width: 100%;
+    transform: translateZ(-30px);
+    border-radius: 27px;
+  }
 `;
 
 export default function Home() {
+  const [xy, setXY] = useState([0, 0]);
+
+  const [ref, bounds] = useMeasure();
+
+  const xPercentage = Math.round(xy[0] - bounds.left) / bounds.width
+  const yPercentage = Math.round(xy[1] - bounds.top) / bounds.height
+  console.log(xPercentage)
+
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: xPercentage,
+  });
+
+  const { y } = useSpring({
+    from: { y: 0 },
+    y: yPercentage,
+
+  });
+
+  const xInterpolated = x.to([0, 0.5, 1], [-20, 0, 20]);
+  const yInterpolated = y.to([0, 0.5, 1], [20, 0, -20]);
+
   return (
     <>
       <Head>
@@ -41,44 +79,56 @@ export default function Home() {
       </Head>
 
       <Grid>
+        <GridItem
+          style={{
+            rotateX: yInterpolated,
+            rotateY: xInterpolated
+          }}
+          ref={ref}
+          onMouseMove={({ clientX, clientY }) => setXY([clientX, clientY])}
+          onMouseLeave={() => setXY([])}
+        >
+          x: {Math.round(xy[0] - bounds.left)}px
+          <br></br>
+          Y: {Math.round(xy[1] - bounds.top)}px
+          <br></br>
+          percentageX: {Math.round(xy[0] - bounds.left) / bounds.width}
+          <br></br>
+          percentageY: {Math.round(xy[1] - bounds.top) / bounds.height}
+        </GridItem>
 
-        <GridItem columnStart={1} columnEnd={3} rowStart={1} rowEnd={2}>yo</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
+        <GridItem></GridItem>
 
-        <GridItem>hey</GridItem>
-
-        <GridItem>hey</GridItem>
-
-
+        <GridItem></GridItem>
       </Grid>
 
-      <footer>
-      </footer>
+      <footer></footer>
     </>
-  )
+  );
 }
